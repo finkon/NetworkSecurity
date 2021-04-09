458ad12f48e1d5e3c66baf3438f18e6d8ff8a946
Bethany Smith <72517674+finkon@users.noreply.github.com>
2021-04-09 21:22:30 +0100

    Add files via upload

---
+++ cyber.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include "header.html"?>
+
+
+  <div class="title">
+  <img class = "center" img src = cyber.png>
+  </div>
+
+  <br>
+  <br>
+
+ <h5>Cyber security report for an SME company <br><br>
+
+  As an SME company you are an attractive target for cyber criminals.They assume you will lack the capability and resources to prevent attacks and deal with the aftermath.<br>
+  
+  SME’s collectively are subject to almost 10,000 cyber-attacks a day. Lets not allow this to happen to your company. </h5>
+
+  <br>
+  
+  
+   <h5>  Some of the risks you may face –</h5>
+   
+   <h2>·Phishing <br>
+     · Data leakage <br>
+     · Hacking<br>
+  
+  · Ransomware<br>
+  · Insider attack  <br>
+  · Malware </h2>
+  
+  <h5>Solutions we can implement –</h5>
+  
+  <h3>· Create a cyber security incident response plan. This is the plan on what to do in the event of a cyber security incident, hopefully if we implement the rest of these points we should never need to use this, but its always necessary to have in place as technology advances and attacks are becoming more proficient everyday<br><br>
+  
+  · Firewalls, these can be customised to include filters specific to your business and needs for instance, you can set the firewall to block a word such as ‘’malicious’<br><br>
+  
+  · Hardware firewall, this unit is considered a gateway and is great for your business as it’s a cheap option and very secure.<br><br>
+  
+  · Firewall – most computers come with an in-built firewall but this is something we can install onto the computers if not. If you use the software then the physical computer becomes the gateway as opposed to the external device above.<br><br>
+  
+  · Malware protection, this scans for viruses, worms and trojans and if detected removes them.<br><br>
+  
+  · Email security, this controls all inbound and outbound activity and detects anything that could be a threat and alerts you of this.<br><br>
+  
+  · Internal training and raising awareness, this is important to keep up with. Send monthly reminder emails out to employees about phishing emails and the threats that surround the business. Make sure they are aware they mustn’t use the business network or computers for personal use and they should never plug anything into the computers that hasn’t come from the business (phone chargers, USB sticks, etc.).<br><br>
+  
+  · Password practices, setup the system to ask you to change your password at least every two months, never write any passwords down anywhere.<br><br>
+  
+  · Restrictions on the network, restrict permission for downloading and installing things and restrict access to certain websites and devices such as Facebook and external phones that are plugged in.
+  </h3>
+ 
+
+  <a href = "home.html"><button class ="center" onclick="form">Back to Home</button></a>
+
+
+
+  <?php include "footer.html"?>
+
+</body>
